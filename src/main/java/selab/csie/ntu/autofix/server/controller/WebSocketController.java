93d2c7339c0f3968691ea07518b2f8620254306e
SE2019Group1 <r07922037@ntu.edu.tw>
2020-06-25 17:52:41 +0800

    send socket ID directly

--- src/main/java/selab/csie/ntu/autofix/server/controller/WebSocketController.java
+++ src/main/java/selab/csie/ntu/autofix/server/controller/WebSocketController.java
@@ -4,7 +4,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import selab.csie.ntu.autofix.server.model.message.WebSocketMessage;
 
 import java.security.Principal;
 
@@ -14,8 +13,8 @@ public class WebSocketController {
 
     @MessageMapping("/whoami")
     @SendToUser("/topic/socket-ID")
-    public WebSocketMessage returnID(Principal principal) {
-        return new WebSocketMessage(principal.getName());
+    public String returnID(Principal principal) {
+        return principal.getName();
     }
 
 }
